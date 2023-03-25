90ea1cef7db7991bac8a864605479f4bce262950
jzstats <jzstats@yandex.com>
2023-03-25 20:30:03 +0200

    added script 'summarize_data.py'

---
+++ summarize_data.py
@@ -0,0 +1,67 @@
+import sqlite3
+
+def get_data():
+    conn = sqlite3.connect('./data/numbersdb.sqlite')
+    cur = conn.cursor()
+    cur.execute("SELECT number FROM Numbers")
+
+    numbers = list()
+    while True:
+        try: 
+            number = cur.fetchone()[0]
+        except: 
+            break
+        numbers.append(number)
+
+    conn.close()
+
+    return numbers
+
+def count_number_occurances(numbers):
+    counts = dict()
+    for number in numbers:
+        counts[number] = counts.get(number, 0) + 1
+    return counts
+
+def sort_counts_descending(counts):
+    def value_getter(item):
+        return item[1]
+    sorted_counts = sorted(counts.items(), key = value_getter, reverse = True)
+    return dict(sorted_counts)
+
+def summarize_numbers(numbers):
+    summary = count_number_occurances(numbers)
+    summary = sort_counts_descending(summary)
+    return summary
+
+def get_top_n_input():
+    while True:
+        n = input("How many top results to show? ")
+        try:
+            n = int(n)
+        except:
+            continue
+        if n < 1: 
+            continue
+        else:
+            break
+    return n
+
+def present_summary(summary):
+    top_n = get_top_n_input()
+    for k, v in summary.items():
+        if top_n == 0: break
+        top_n = top_n - 1
+        if v == 1:
+            print(str(k) + " occured 1 time")
+        else:
+            print(str(k) + " occured " + str(v) + " times")
+# Read the Processed Data in python
+numbers = get_data()
+
+# Summarize occurances of each number
+summary = summarize_numbers(numbers)
+
+# Present results 
+present_summary(summary)
+        
