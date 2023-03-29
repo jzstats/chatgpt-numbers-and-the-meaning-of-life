32d394251ef01fa3b61bd7dd74ced5dcaacabfec
jzstats <jzstats@yandex.com>
2023-03-29 22:35:02 +0300

    refactoring 'process_raw_data.py'

--- process_raw_data.py
+++ process_raw_data.py
@@ -13,39 +13,57 @@ def init_table_with_processed_data(cur):
 def get_total_number_of_responses(cur):
     cur.execute("SELECT COUNT(response) FROM Responses")
     return cur.fetchone()[0]
-    
-conn_1 = sqlite3.connect('./data/numbersdb.sqlite')
-cur_1 = conn_1.cursor()
 
-init_table_with_processed_data(cur_1)
-
-conn_2 = sqlite3.connect('./data/rawdb.sqlite')
-cur_2 = conn_2.cursor()
-
-n_responses = get_total_number_of_responses(cur_2)
-print(n_responses)
-cur_2.execute("SELECT response FROM Responses")
-for i in range(n_responses):
-# for i in range(1000):
-    response = cur_2.fetchone()[0]
-    # Clean
+def clean_raw_data(response):
+    # Eliminate commas from everywhere
     response = response.replace(",", "")
+    # Eliminate trailing dots everywhere
     response = re.sub("(\.)$", "", response)
+    # Transform twelve to from text to number 12
     response = re.sub("^twelve$", "12", response)
-    # Extract
-    response = re.findall(r'\d+\.?\d*', response)[0]
-    # Verify
-    # if this doesn't blow everything is fine
+    return response
+
+def extract_target_data(response):
+    return re.findall(r'\d+\.?\d*', response)[0]
+
+def validate_data(response):
+    # If everything was correct it should be possible
+    # to convert each string to interger or float
+    # In case it fails, data needs further inspection and cleaning
     try: 
         response = int(response)
     except:
         response = round(float(response), 2)
     # Convert back to string
-    response = str(response)
-    print(response)
-    # Insert number to DB and commit changes
-    cur_1.execute('INSERT INTO Numbers (number) VALUES ( ? )', ( response, ))
-    conn_1.commit()
+    return str(response)
+    
+def process_raw_data():
+    # Establish connection to handle the clean data DB 
+    conn_1 = sqlite3.connect('./data/numbersdb.sqlite')
+    cur_1 = conn_1.cursor()
+    # DELETEs and recreate the table for the number
+    init_table_with_processed_data(cur_1)
+    # Establish connection to the raw data DB
+    conn_2 = sqlite3.connect('./data/rawdb.sqlite')
+    cur_2 = conn_2.cursor()
 
-conn_1.close()
-conn_2.close()
+    n_responses = get_total_number_of_responses(cur_2)
+    if n_responses < 1:
+        print("There are no numbers to clean.")
+    else:
+        print("There are " + n_responses + " numbers to clean.")
+        cur_2.execute("SELECT response FROM Responses")
+        for i in range(n_responses):
+            raw_response = cur_2.fetchone()[0]
+            clean_response = clean_raw_data(raw_response)
+            number = extract_target_data(clean_response)
+            number = validate_data(number)
+            # if this doesn't blow everything is fine
+            print(number)
+            # Insert number to DB and commit changes
+            cur_1.execute('INSERT INTO Numbers (number) VALUES ( ? )', ( number, ))
+            conn_1.commit()
+
+        conn_1.close()
+        conn_2.close()
+    return True
