8e5a2e0db442188a4775a90acc3c07cd2b85687e
jzstats <jzstats@yandex.com>
2023-03-30 04:45:58 +0300

    modified 'summarize_results.py'

--- summarize_results.py
+++ summarize_results.py
@@ -17,6 +17,14 @@ def data_getter():
 
     return numbers
 
+def get_total_number_of_entries():
+    conn = sqlite3.connect('./data/numbersdb.sqlite')
+    cur = conn.cursor()
+    cur.execute("SELECT COUNT(number) FROM Numbers")
+    n = cur.fetchone()[0]
+    conn.close()
+    return int(n)
+
 def count_number_occurances(numbers):
     counts = dict()
     for number in numbers:
@@ -34,28 +42,40 @@ def summarize_numbers(numbers):
     summary = sort_counts_descending(summary)
     return summary
 
-def get_top_n_input():
+def input_getter():
     while True:
-        n = input("How many top results to show? ")
+        try: 
+            n = input("How many top results to show? (q to quit) ")
+        except KeyboardInterrupt:
+            print('\nUser Interrupt.')
+            exit()
+        if n == 'q' : exit()
         try:
             n = int(n)
         except:
+            print('Unexpected input. Try again.')
             continue
         if n < 1: 
+            print('..invalid input. Give a non-negatige number.')
             continue
         else:
             break
     return n
 
 def present_summary(summary):
-    top_n = get_top_n_input()
+    top_n = input_getter()
+    n = get_total_number_of_entries()
+    print("Out of " + str(n) + " numbers: ")
+    aggr_perc = 0
     for k, v in summary.items():
-        if top_n == 0: break
+        if top_n < 0: break
         top_n = top_n - 1
-        if v == 1:
-            print(str(k) + " occured 1 time")
-        else:
-            print(str(k) + " occured " + str(v) + " times")
+        perc = round(float(v)/n*100, 2)
+        aggr_perc = aggr_perc + perc
+        print(str(k) + " occured " + str(perc) + "% of times")
+
+    print('\n' + 'Aggregated the  numbers cover ' + str(round(aggr_perc,2)) + '% of all observations.')
+    return aggr_perc
 
 
 def summarize_results():
